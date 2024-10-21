82da44dd5377a6d1558fc478737188ce518a3d2a
Jayesh Velayudhan <jayesh.mail@gmail.com>
2024-10-21 19:20:05 +0200

    Move scripts to a different directory

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/zsh
 
 # My Sway startup script
-swaymsg workspace 1
-google-chrome &
-sleep 0.2
-
 swaymsg workspace 2
 alacritty --title="   emacsclient" -e emacsclient -t &
 sleep 0.2
@@ -13,10 +9,13 @@ swaymsg workspace 3
 alacritty &
 sleep 0.1
 
-swaymsg workspace 4
-firefox &
-sleep 0.2
-
 swaymsg workspace 9
 alacritty --title="   calendar" -e calcurse &
 sleep 0.1
+
+swaymsg workspace 4
+firefox &
+sleep 0.5
+
+swaymsg workspace 1
+google-chrome &
