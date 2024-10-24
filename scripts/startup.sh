46cd912ddc4c8031691d7229d00015ed5fd1e887
Jayesh Velayudhan <jayesh.mail@gmail.com>
2024-10-24 18:07:56 +0200

    Add scripts for blog

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/zsh
 
 # My Sway startup script
-swaymsg workspace 2
+swaymsg workspace 1
 alacritty --title="   emacsclient" -e emacsclient -t &
-sleep 0.2
+sleep 0.5
 
 swaymsg workspace 3
 alacritty &
-sleep 0.1
-
-swaymsg workspace 9
-alacritty --title="   calendar" -e calcurse &
 sleep 0.1
 
 swaymsg workspace 4
 firefox &
 sleep 0.5
 
-swaymsg workspace 1
+swaymsg workspace 9
+alacritty --title="   calendar" -e calcurse &
+sleep 0.1
+
+swaymsg workspace 2
 google-chrome &
